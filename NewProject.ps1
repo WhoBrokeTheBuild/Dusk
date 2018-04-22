e182f5d8d713c9204319284f964ef8c2921b3757
Stephen Lane-Walsh <sdl.slane@gmail.com>
2018-04-21 21:20:39 -0400

    Updating things for windows

--- NewProject.ps1
+++ NewProject.ps1
@@ -1,4 +1,19 @@
+function Test-Administrator
+{
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
-New-ItemProperty -Path "HKLM:\Software\Dusk" -Name "Path" -Value "$PSScriptRoot"
+if (!(Test-Administrator))
+{
+    Write-Host "Run this script as an Administrator"
+    Exit
+}
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+if (!(Test-Path "HKLM:\Software\Dusk")) {
+    New-ItemProperty -Path "HKLM:\Software\Dusk" -Name "Path" -Value "$PSScriptRoot"
+}
 
 Copy-Item "$PSScriptRoot\cmake\Project.cmake" "CmakeLists.txt"

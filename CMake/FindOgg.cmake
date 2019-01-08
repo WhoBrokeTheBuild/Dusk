ada21e50faef9617bfe3bf64814f0da0d1c29981
Stephen Lane-Walsh <sdl.slane@gmail.com>
2019-01-08 18:04:19 -0500

    Use PkgConfig better

--- CMake/FindOgg.cmake
+++ CMake/FindOgg.cmake
@@ -30,54 +30,62 @@
 FIND_PACKAGE(PkgConfig QUIET)
 PKG_CHECK_MODULES(_Ogg_PC QUIET ogg)
 PKG_CHECK_MODULES(_Vorbis_PC QUIET vorbis)
+PKG_CHECK_MODULES(_VorbisFile_PC QUIET vorbisfile)
 PKG_CHECK_MODULES(_Theora_PC QUIET theora)
 
+SET(Ogg_INCLUDE_DIR ${_Ogg_PC_INCLUDE_DIRS})
 FIND_PATH(
     Ogg_INCLUDE_DIR
     NAMES ogg/ogg.h
-    PATHS ${Ogg_ROOT_DIR} ${_Ogg_PC_INCLUDE_DIRS} 
+    PATHS ${Ogg_ROOT_DIR}
     PATH_SUFFIXES include
 )
 
+SET(Vorbis_INCLUDE_DIR ${_Vorbis_PC_INCLUDE_DIRS})
 FIND_PATH(
     Vorbis_INCLUDE_DIR
     NAMES vorbis/vorbisfile.h
-    PATHS ${Vorbis_ROOT_DIR} ${_Vorbis_PC_INCLUDE_DIRS} 
+    PATHS ${Vorbis_ROOT_DIR}
     PATH_SUFFIXES include
 )
 
+SET(Theora_INCLUDE_DIR ${_Theora_PC_INCLUDE_DIRS})
 FIND_PATH(
     Theora_INCLUDE_DIR
     NAMES theora/theora.h
-    PATHS ${Theora_ROOT_DIR} ${_Theora_PC_INCLUDE_DIRS} 
+    PATHS ${Theora_ROOT_DIR}
     PATH_SUFFIXES include
 )
 
+SET(Ogg_LIBRARY ${_Ogg_PC_LIBRARIES})
 FIND_LIBRARY(
     Ogg_LIBRARY
     NAMES ogg libogg_static
-    PATHS ${Ogg_ROOT_DIR} ${_Ogg_PC_INCLUDE_DIRS} 
+    PATHS ${Ogg_ROOT_DIR}
     PATH_SUFFIXES lib
 )
 
+SET(Vorbis_LIBRARY ${_Vorbis_PC_LIBRARIES})
 FIND_LIBRARY(
     Vorbis_LIBRARY
     NAMES vorbis libvorbis_static
-    PATHS ${Vorbis_ROOT_DIR} ${_Vorbis_PC_INCLUDE_DIRS} 
+    PATHS ${Vorbis_ROOT_DIR}
     PATH_SUFFIXES lib
 )
 
+SET(VorbisFile_LIBRARY ${_VorbisFile_PC_LIBRARIES})
 FIND_LIBRARY(
     VorbisFile_LIBRARY
     NAMES vorbisfile libvorbisfile_static
-    PATHS ${Vorbis_ROOT_DIR} ${_Vorbis_PC_INCLUDE_DIRS} 
+    PATHS ${Vorbis_ROOT_DIR}
     PATH_SUFFIXES lib
 )
 
+SET(Theora_LIBRARY ${_Theora_PC_LIBRARIES})
 FIND_LIBRARY(
     Theora_LIBRARY
     NAMES theora libtheora_static
-    PATHS ${Theora_ROOT_DIR} ${_Theora_PC_INCLUDE_DIRS} 
+    PATHS ${Theora_ROOT_DIR}
     PATH_SUFFIXES lib
 )
 

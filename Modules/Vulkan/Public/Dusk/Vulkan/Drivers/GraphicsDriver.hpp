c573def859591f4e38d9ae35881a9d89359e557b
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2019-12-03 16:34:53 -0500

    Feature(Vulkan): Began adding Vulkan support
    Update glad for OpenGL and Vulkan
    Add support for auto-configuring .in files in Modules
    Add Dusk Version information to the Core Config.hpp

---
+++ Modules/Vulkan/Public/Dusk/Vulkan/Drivers/GraphicsDriver.hpp
@@ -0,0 +1,64 @@
+#ifndef DUSK_VULKAN_GRAPHICS_DRIVER_HPP
+#define DUSK_VULKAN_GRAPHICS_DRIVER_HPP
+
+#include <Dusk/Vulkan/Config.hpp>
+#include <Dusk/Drivers/GraphicsDriver.hpp>
+
+#include <SDL.h>
+#include <glad/vulkan.h>
+
+#include <vector>
+
+namespace Dusk::Vulkan {
+
+class DUSK_VULKAN_API GraphicsDriver : public IGraphicsDriver
+{
+public:
+
+    GraphicsDriver();
+
+    virtual ~GraphicsDriver();
+
+    void SetWindowTitle(const std::string& title) override;
+
+    std::string GetWindowTitle() override;
+
+    void SetWindowSize(const ivec2& size) override;
+
+    ivec2 GetWindowSize() override;
+
+    void ProcessEvents() override;
+
+    void SwapBuffers() override;
+
+private:
+
+    SDL_Window * _sdlWindow = nullptr;
+
+    VkInstance _vkInstance;
+
+    VkSurfaceKHR _vkWindowSurface;
+
+    VkPhysicalDevice _vkPhysicalDevice;
+
+    VkDevice _vkDevice;
+
+    VkQueue _vkGraphicsQueue;
+    VkQueue _vkPresentQueue;
+
+    VkSemaphore _vkImageAvailableSemaphore;
+    VkSemaphore _vkRenderingFinishedSemaphore;
+
+    VkSwapchainKHR _vkSwapChain;
+
+    std::vector<VkImage> _vkSwapChainImages;
+
+    VkCommandPool _vkCommandPool;
+
+    std::vector<VkCommandBuffer> _vkPresentCommandBuffers;
+
+}; // class GraphicsDriver
+
+} // namespace Dusk::Vulkan
+
+#endif // DUSK_VULKAN_GRAPHICS_DRIVER_HPP

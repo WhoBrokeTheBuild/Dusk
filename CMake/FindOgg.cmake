40a2fdbaff6502eaf063017dca0413dc5d32236c
Stephen Lane-Walsh <sdl.slane@gmail.com>
2019-12-02 21:00:07 -0500

    Fix(Modules): Fix LD_LIBRARY_PATH for dlopen
    Mask missing RUNTIME_PATHs for FindOpenAL and FindSDL2
    Add Vorbis and Theora COMPONENTS to FindOgg
    Standardize on RUNTIME_SEARCH_PATH for setting CMAKE_MSVC_RUN_PATH and
    LD_LIBRARY_PATH
    Only look for GTest when BUILD_TESTS is on
    Set minimum CMake version to 3.12 to fix Travis

--- CMake/FindOgg.cmake
+++ CMake/FindOgg.cmake
@@ -2,6 +2,11 @@
 #
 # Finds Xiph.org's Ogg, Vorbis, and Theora Libraries
 #
+# The following components are supported
+#
+#   Vorbis
+#   Theora
+#
 # This will define the following variables
 #
 #   Ogg_FOUND
@@ -28,32 +33,14 @@
 #
 
 FIND_PACKAGE(PkgConfig QUIET)
+
 PKG_CHECK_MODULES(_Ogg_PC QUIET ogg)
-PKG_CHECK_MODULES(_Vorbis_PC QUIET vorbis)
-PKG_CHECK_MODULES(_VorbisFile_PC QUIET vorbisfile)
-PKG_CHECK_MODULES(_Theora_PC QUIET theora)
 
 SET(Ogg_INCLUDE_DIR ${_Ogg_PC_INCLUDE_DIRS})
 FIND_PATH(
     Ogg_INCLUDE_DIR
     NAMES ogg/ogg.h
     PATHS ${Ogg_ROOT_DIR}
-    PATH_SUFFIXES include
-)
-
-SET(Vorbis_INCLUDE_DIR ${_Vorbis_PC_INCLUDE_DIRS})
-FIND_PATH(
-    Vorbis_INCLUDE_DIR
-    NAMES vorbis/vorbisfile.h
-    PATHS ${Vorbis_ROOT_DIR}
-    PATH_SUFFIXES include
-)
-
-SET(Theora_INCLUDE_DIR ${_Theora_PC_INCLUDE_DIRS})
-FIND_PATH(
-    Theora_INCLUDE_DIR
-    NAMES theora/theora.h
-    PATHS ${Theora_ROOT_DIR}
     PATH_SUFFIXES include
 )
 
@@ -65,41 +52,70 @@ FIND_LIBRARY(
     PATH_SUFFIXES lib
 )
 
-SET(Vorbis_LIBRARY ${_Vorbis_PC_LIBRARIES})
-FIND_LIBRARY(
-    Vorbis_LIBRARY
-    NAMES vorbis libvorbis_static
-    PATHS ${Vorbis_ROOT_DIR}
-    PATH_SUFFIXES lib
-)
+IF(Ogg_FIND_REQUIRED_Vorbis OR Ogg_FIND_REQUIRED_Theora)
+    PKG_CHECK_MODULES(_Vorbis_PC QUIET vorbis)
+    PKG_CHECK_MODULES(_VorbisFile_PC QUIET vorbisfile)
 
-SET(VorbisFile_LIBRARY ${_VorbisFile_PC_LIBRARIES})
-FIND_LIBRARY(
-    VorbisFile_LIBRARY
-    NAMES vorbisfile libvorbisfile_static
-    PATHS ${Vorbis_ROOT_DIR}
-    PATH_SUFFIXES lib
-)
+    SET(Vorbis_INCLUDE_DIR ${_Vorbis_PC_INCLUDE_DIRS})
+    FIND_PATH(
+        Vorbis_INCLUDE_DIR
+        NAMES vorbis/vorbisfile.h
+        PATHS ${Vorbis_ROOT_DIR}
+        PATH_SUFFIXES include
+    )
 
-SET(Theora_LIBRARY ${_Theora_PC_LIBRARIES})
-FIND_LIBRARY(
-    Theora_LIBRARY
-    NAMES theora libtheora_static
-    PATHS ${Theora_ROOT_DIR}
-    PATH_SUFFIXES lib
-)
+    SET(Vorbis_LIBRARY ${_Vorbis_PC_LIBRARIES})
+    FIND_LIBRARY(
+        Vorbis_LIBRARY
+        NAMES vorbis libvorbis_static
+        PATHS ${Vorbis_ROOT_DIR}
+        PATH_SUFFIXES lib
+    )
+
+    SET(VorbisFile_LIBRARY ${_VorbisFile_PC_LIBRARIES})
+    FIND_LIBRARY(
+        VorbisFile_LIBRARY
+        NAMES vorbisfile libvorbisfile_static
+        PATHS ${Vorbis_ROOT_DIR}
+        PATH_SUFFIXES lib
+    )
+
+    IF(Vorbis_INCLUDE_DIR AND Vorbis_LIBRARY AND VorbisFile_LIBRARY)
+        SET(Ogg_Vorbis_FOUND TRUE)
+    ENDIF()
+ENDIF()
+
+IF(Ogg_FIND_REQUIRED_Theora)
+    PKG_CHECK_MODULES(_Theora_PC QUIET theora)
+
+    SET(Theora_INCLUDE_DIR ${_Theora_PC_INCLUDE_DIRS})
+    FIND_PATH(
+        Theora_INCLUDE_DIR
+        NAMES theora/theora.h
+        PATHS ${Theora_ROOT_DIR}
+        PATH_SUFFIXES include
+    )
+
+    SET(Theora_LIBRARY ${_Theora_PC_LIBRARIES})
+    FIND_LIBRARY(
+        Theora_LIBRARY
+        NAMES theora libtheora_static
+        PATHS ${Theora_ROOT_DIR}
+        PATH_SUFFIXES lib
+    )
+
+    IF(Theora_INCLUDE_DIR AND Theora_LIBRARY)
+        SET(Ogg_Theora_FOUND TRUE)
+    ENDIF()
+ENDIF()
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
     Ogg
+    HANDLE_COMPONENTS
     REQUIRED_VARS 
         Ogg_INCLUDE_DIR
-        Ogg_LIBRARY 
-        Vorbis_INCLUDE_DIR
-        Vorbis_LIBRARY 
-        VorbisFile_LIBRARY 
-        Theora_INCLUDE_DIR
-        Theora_LIBRARY 
+        Ogg_LIBRARY
 )
 
 ADD_LIBRARY(Ogg::Ogg INTERFACE IMPORTED)

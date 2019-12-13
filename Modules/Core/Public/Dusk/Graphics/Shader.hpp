b552eb094b8b7b842e0248ebb859b8630137b43d
Stephen Lane-Walsh <sdl.slane@gmail.com>
2019-12-13 00:43:14 -0500

    Chore(Build): Auto-generate .spv files from .glsl
    Add FindVulkanSDK to find glslangValidator program
    Remove ShaderImporters

--- Modules/Core/Public/Dusk/Graphics/Shader.hpp
+++ Modules/Core/Public/Dusk/Graphics/Shader.hpp
@@ -2,11 +2,36 @@
 #define DUSK_SHADER_HPP
 
 #include <Dusk/Config.hpp>
+#include <Dusk/Macros.hpp>
+
+#include <unordered_map>
 
 namespace Dusk {
 
+struct DUSK_CORE_API ShaderData
+{
+    DISALLOW_COPY_AND_ASSIGN(ShaderData)
+
+    explicit ShaderData() = default;
+
+    ShaderData(ShaderData&& rhs) {
+        std::swap(Files, rhs.Files);
+    }
+
+    std::unordered_map<std::string, std::string> Files;
+};
+
 class DUSK_CORE_API IShader 
 {
+public:
+
+    DISALLOW_COPY_AND_ASSIGN(IShader)
+
+    explicit IShader() = default;
+
+    virtual ~IShader() = default;
+
+    virtual void Bind() = 0;
 
 };
 
---
+++ Modules/OpenGL/Public/Dusk/OpenGL/Shader.hpp
@@ -0,0 +1,20 @@
+#ifndef DUSK_OPENGL_SHADER_HPP
+#define DUSK_OPENGL_SHADER_HPP
+
+#include <Dusk/OpenGL/Config.hpp>
+#include <Dusk/Graphics/Shader.hpp>
+
+namespace Dusk {
+
+class DUSK_OPENGL_API Shader : public IShader 
+{
+public:
+
+
+
+
+}; // class Shader
+
+} // namespace Dusk
+
+#endif // DUSK_OPENGL_SHADER_HPP

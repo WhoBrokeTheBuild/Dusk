a86a75ef32b1abe7d26c1fd203f171d7cbf0f1a1
Stephen Lane-Walsh <sdl.slane@gmail.com>
2020-01-19 15:40:59 -0500

    Work on Vulkan. Renamed Folders

--- Demos/HelloWorld/Scripts/Main.py
+++ Demos/HelloWorld/Scripts/Main.py
@@ -4,6 +4,7 @@ Dusk.LoadModule("DuskSTBI")
 Dusk.LoadModule("DuskTinyOBJ")
 # Dusk.LoadModule("DuskFreeType")
 Dusk.LoadModule("DuskOpenGL")
+# Dusk.LoadModule("DuskVulkan")
 
 gfx = Dusk.GetGraphicsDriver()
 gfx.SetWindowSize(( 1024, 768 ))

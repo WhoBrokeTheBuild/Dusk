0a33c79ca9cb1616df43cb186324cf5f5b6580ca
Stephen Lane-Walsh <sdl.slane@gmail.com>
2020-01-27 22:57:11 -0500

    Remove Old

--- Old/Engine/include/dusk/Main.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DUSK_MAIN_HPP
-#define DUSK_MAIN_HPP
-
-#include <dusk/Dusk.hpp>
-
-typedef void (*DuskSetupFn)(dusk::App *);
-extern "C" void _DuskSetup(dusk::App * app);
-
-#endif // DUSK_MAIN_HPP
--- Old/Engine/include/dusk/core/Macros.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef DUSK_MACROS_HPP
-#define DUSK_MACROS_HPP
-
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-    TypeName(const TypeName&) = delete;   \
-    TypeName& operator=(const TypeName&) = delete;
-
-#endif // DUSK_MACROS_HPP

a1b7d51ef01adb1e71f36332aa3f4dd9720f1268
Stephen Lane-Walsh <sdl.slane@gmail.com>
2020-05-02 14:47:38 -0400

    Chore(Test): Fix STBI Test git lfs updates

--- Modules/STBI/Tests/STBI_TextureImporter_Test.cpp
+++ Modules/STBI/Tests/STBI_TextureImporter_Test.cpp
@@ -4,84 +4,81 @@
 
 #include "test.h"
 
-using namespace Dusk;
-using namespace Dusk::STBI;
-
-static TextureImporter _TextureImporter;
+static Dusk::STBI::TextureImporter _TextureImporter;
 
 TEST(LoadFromFile, PNG_RGB) {
     auto data = _TextureImporter.LoadFromFile("test_rgb.png");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 
 TEST(LoadFromFile, PNG_RGBA) {
     auto data = _TextureImporter.LoadFromFile("test_rgba.png");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 4);
 }
 
 TEST(LoadFromFile, PNG_Grey) {
     auto data = _TextureImporter.LoadFromFile("test_grey.png");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 1);
 }
 
 TEST(LoadFromFile, JPEG_RGB) {
     auto data = _TextureImporter.LoadFromFile("test_rgb.jpg");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 
 TEST(LoadFromFile, JPEG_Grey) {
     auto data = _TextureImporter.LoadFromFile("test_grey.jpg");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 1);
 }
 
 TEST(LoadFromFile, BMP_RGB) {
     auto data = _TextureImporter.LoadFromFile("test_rgb.bmp");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 
 TEST(LoadFromFile, BMP_Grey) {
     auto data = _TextureImporter.LoadFromFile("test_rgb.bmp");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 
 TEST(LoadFromFile, TGA) {
     auto data = _TextureImporter.LoadFromFile("test_rgb.tga");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 TEST(LoadFromFile, GIF_RGB) {
     auto data = _TextureImporter.LoadFromFile("test_rgb.gif");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 4);
 }
 
 TEST(LoadFromFile, GIF_RGBA) {
     auto data = _TextureImporter.LoadFromFile("test_rgba.gif");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 4);
 }
 
 TEST(LoadFromFile, GIF_Grey) {
     auto data = _TextureImporter.LoadFromFile("test_grey.gif");
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 4);
 }
 
@@ -97,29 +94,29 @@ TEST(LoadFromFile, FileNotFound) {
 
 TEST(LoadFromMemory, PNG_RGB) {
     auto data = _TextureImporter.LoadFromMemory(___Modules_STBI_Tests_test_png, ___Modules_STBI_Tests_test_png_len);
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 
 TEST(LoadFromMemory, JPEG_RGB) {
     auto data = _TextureImporter.LoadFromMemory(___Modules_STBI_Tests_test_jpg, ___Modules_STBI_Tests_test_jpg_len);
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 
 TEST(LoadFromMemory, BMP_RGB) {
     auto data = _TextureImporter.LoadFromMemory(___Modules_STBI_Tests_test_bmp, ___Modules_STBI_Tests_test_bmp_len);
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 
 TEST(LoadFromMemory, TGA_RGB) {
     auto data = _TextureImporter.LoadFromMemory(___Modules_STBI_Tests_test_tga, ___Modules_STBI_Tests_test_tga_len);
-    EXPECT_FALSE(data == nullptr);
-    EXPECT_EQ(data->GetSize(), uvec2(32, 32));
+    ASSERT_FALSE(data == nullptr);
+    EXPECT_EQ(data->GetSize(), Dusk::uvec2(32, 32));
     EXPECT_EQ(data->GetComponents(), 3);
 }
 

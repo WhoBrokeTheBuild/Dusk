39b6d01ff7c69e2a83885f9b619e8f54e927e1e2
Stephen Lane-Walsh <sdl.slane@gmail.com>
2020-11-30 23:09:23 -0500

    Add automatic generation of vs code debug targets

--- CMake/DefineDemo.cmake
+++ CMake/DefineDemo.cmake
@@ -164,12 +164,12 @@ MACRO(DEFINE_DEMO _target)
             ${_target}
             PROPERTIES 
                 VS_DEBUGGER_WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                VS_DEBUGGER_ENVIRONMENT "PATH=${RUNTIME_SEARCH_PATH};$<$<CONFIG:Debug>:${RUNTIME_SEARCH_PATH_DEBUG};>$<$<CONFIG:Release>:${RUNTIME_SEARCH_PATH_RELEASE};>%PATH%\nDUSK_ASSET_PATH=${ASSET_PATH}"
+                VS_DEBUGGER_ENVIRONMENT "PATH=${RUNTIME_SEARCH_PATH};$<$<CONFIG:Debug>:${RUNTIME_SEARCH_PATH_DEBUG};>$<$<CONFIG:Release>:${RUNTIME_SEARCH_PATH_RELEASE};>$ENV{PATH}\nDUSK_ASSET_PATH=${ASSET_PATH}"
         )
 
         ADD_CUSTOM_TARGET(
             run-${_target}
-            COMMAND ${CMAKE_COMMAND} -E env "PATH=${RUNTIME_SEARCH_PATH};$<$<CONFIG:Debug>:${RUNTIME_SEARCH_PATH_DEBUG};>$<$<CONFIG:Release>:${RUNTIME_SEARCH_PATH_RELEASE};>%PATH%" "DUSK_ASSET_PATH=${ASSET_PATH}" $<TARGET_FILE:${_target}>
+            COMMAND ${CMAKE_COMMAND} -E env "PATH=${RUNTIME_SEARCH_PATH};$<$<CONFIG:Debug>:${RUNTIME_SEARCH_PATH_DEBUG};>$<$<CONFIG:Release>:${RUNTIME_SEARCH_PATH_RELEASE};>$ENV{PATH}" "DUSK_ASSET_PATH=${ASSET_PATH}" $<TARGET_FILE:${_target}>
             DEPENDS ${_target}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
@@ -180,6 +180,15 @@ MACRO(DEFINE_DEMO _target)
                 FOLDER "Automation"
         )
 
+        # Append debug target to .vscode/launch.json if .vscode/ exists
+        IF(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/.vscode)
+            SET(_launch_json ${CMAKE_SOURCE_DIR}/.vscode/launch.json)
+            ADD_CUSTOM_COMMAND(
+                TARGET ${_target} POST_BUILD
+                BYPRODUCTS ${_launch_json}
+                COMMAND ${Python3_EXECUTABLE} ${CMAKE_SOURCE_DIR}/Scripts/add-vscode-launch-target.py ${_launch_json} "${_target} ($<CONFIG>)" $<TARGET_FILE:${_target}> ${CMAKE_CURRENT_SOURCE_DIR} "PATH=${RUNTIME_SEARCH_PATH};$<$<CONFIG:Debug>:${RUNTIME_SEARCH_PATH_DEBUG};>$<$<CONFIG:Release>:${RUNTIME_SEARCH_PATH_RELEASE};>$ENV{PATH}" "DUSK_ASSET_PATH=${ASSET_PATH}"
+            )
+        ENDIF()
     ELSE()
         STRING(JOIN ":" LD_LIBRARY_PATH ${RUNTIME_SEARCH_PATH})
         STRING(JOIN ":" ASSET_PATH ${ASSET_PATH})
@@ -226,5 +235,15 @@ MACRO(DEFINE_DEMO _target)
                     FOLDER "Automation"
             )
         ENDIF()
+
+        IF(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/.vscode)
+            SET(_launch_json ${CMAKE_SOURCE_DIR}/.vscode/launch.json)
+            ADD_CUSTOM_COMMAND(
+                TARGET ${_target} POST_BUILD
+                BYPRODUCTS ${_launch_json}
+                COMMAND ${Python3_EXECUTABLE} ${CMAKE_SOURCE_DIR}/Scripts/add-vscode-launch-target.py ${_launch_json} ${_target} $<TARGET_FILE:${_target}> ${CMAKE_CURRENT_SOURCE_DIR} "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" "DUSK_ASSET_PATH=${ASSET_PATH}"
+            )
+        ENDIF()
     ENDIF()
+    
 ENDMACRO()
